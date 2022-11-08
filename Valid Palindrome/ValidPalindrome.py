fe0082fa061369771f26835d1c556cf8ccb70147
Brandon <branyzp@gmail.com>
2022-11-08 20:37:56 +0800

    added is valid palindrome with another method

--- Valid Palindrome/ValidPalindrome.py
+++ Valid Palindrome/ValidPalindrome.py
@@ -27,22 +27,30 @@
 # s consists only of printable ASCII characters.
 
 # first method using python methods
-def isPalindrome(s):
-    # create new string and add a char into new string if it is alphanumeric (aA -zZ, 0-9) in lowercase
-    newS = ""
-    for char in s:
-        if char.isalnum():
-            newS += char.lower()
-
-    # return boolean value of if new string is the same backwards
-    return newS == newS[::-1]
-            
-
-
-# second method ()
 # def isPalindrome(s):
-#     l,r = 0,len(s)-1
+#     # create new string and add a char into new string if it is alphanumeric (aA -zZ, 0-9) in lowercase
+#     newS = ""
+#     for char in s:
+#         if char.isalnum():
+#             newS += char.lower()
 
+#     # return boolean value of if new string is the same backwards
+#     return newS == newS[::-1]
+
+
+# second method (2 pointers at each end)
+def isPalindrome(s):
+    l, r = 0, len(s)-1
+
+    while l < r:
+        while l < r and s[l].isalnum() == False:
+            l += 1
+        while r > l and s[r].isalnum() == False:
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l, r = l+1, r-1
+    return True
 
 
 s = "A man, a plan, a canal: Panama"
